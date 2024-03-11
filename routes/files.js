53fbd330d40c3a739d9645a9614ab2a0cbbce089
Rabbilkhan <rabbilkhan125@gmail.com>
2024-03-11 16:12:56 +0530

    email code added

--- routes/files.js
+++ routes/files.js
@@ -51,4 +51,49 @@ size:req.file.size
   //Response -> Link
 });
 
+router.post('/send', async(req,res)=>{
+  //validate request
+
+const{uuid, emailTo, emailFrom} =  req.body;
+
+
+//validate request
+
+if(!uuid || !emailTo || !emailFrom){
+  return res.status(422).send({error: 'All fields are required'});
+}
+
+//Get data from database
+
+const file = await File.findOne({uuid:uuid});
+
+if(file.sender){
+  return res.status(422).send({error: 'Email already send.'});
+}
+
+file.sender = emailFrom;
+file.receiver = emailTo;
+
+const response = await file.save();
+
+//send email
+
+const sendMail = require('../services/emailService')
+sendMail({
+  from:emailFrom,
+    to:emailTo,
+    subject:'file sharing App',
+    text:`${emailFrom} shared a file with you`,
+    html: require('../services/emailTemplate')({
+      emailFrom: emailFrom,
+      downloadLink:`${process.env.APP_BASE_URL}/files/${file.uuid}`,
+      size: parseInt(file.size/1000) + 'KB',
+      expires:'24 hours'
+    })
+});
+return res.send({success :'Email sent Succesfully!'})
+
+})
+
+
 module.exports = router;
