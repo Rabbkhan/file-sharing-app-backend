94761bc5fd4a72e101aa6e477673c27e3dd36051
Rabbilkhan <rabbilkhan125@gmail.com>
2024-03-15 18:57:16 +0530

    file upload  forntend code added

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -147,7 +147,7 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">inShare inc. main street 29, Moscow</span>
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">share , haldia</span>
                                 <br> Want to share a file? <a href="http://localhost:5147" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
                             </td>
                             </tr>
